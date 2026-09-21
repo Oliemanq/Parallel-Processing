9c83d865cdaf80c4eda01f1c26e946d9e2d41ea4
Oliemanq <oliemanq456@gmail.com>
2026-09-21 08:27:18 -0500

    testing

--- test.c
+++ test.c
@@ -16,4 +16,6 @@ int main(int argc, char* argv[]) { //argc takes number of arguments, argv holds 
 
     int functionVal = functionOne();
     printf("%f", functionVal);
+
+    printf("changing stuff");
 }
